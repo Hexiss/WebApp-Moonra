ae68c198c73f128e882c064008ed82d25c4cf046
0ri0n09 <jonathan.jaremczuk@ynov.com>
2022-03-09 13:54:07 +0100

    search

---
+++ src/views/Search.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+    <div>
+        
+    </div>
+
+    <div class="grid grid-cols-2">
+    <div>
+        <router-link to="#">
+            <img class="" src="@/assets/images/Videos.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="" src="@/assets/images/Podcasts.png" alt="">
+        </router-link>
+    </div>
+    </div>
+
+    <div class="grid grid-cols-4 gap-2">
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Yoga.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Fitness.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Meditation.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Sophrologie.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Yoga.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Fitness.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Meditation.png" alt="">
+        </router-link>
+    </div>
+    <div>
+        <router-link to="#">
+            <img class="mx-auto w-auto" src="@/assets/images/Sophrologie.png" alt="">
+        </router-link>
+    </div>
+    
+    </div>
+</div>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
