7077c5ee2a3773abe1a02fe439feaf43f91a6985
Wakidou <wakidou.abdallah@gmail.com>
2022-03-09 14:01:45 +0100

    fix error App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
   <div>
     <Navbar />
     <RouterView class="view" />
-</header>
-
-<footer>
-<Footer />
-</footer>
+    <Footer />
 
 </template>
 
@@ -26,21 +22,11 @@ import Footer from "@/components/Footer.vue";
 <style>
 @import "@/assets/base.css";
 
-<<<<<<< HEAD
-.view
-{
-  padding: 0 10%;
-}
-=======
-<<<<<<< HEAD
-  .view {
+.view {
     padding: 5% 10%;
   }
 
-=======
 .view{
   height: 50vh;
 }
->>>>>>> 60e6641ab5a9bfb4da6491d5a1e77be634111074
->>>>>>> d41f5f5f2a66d7f0be484be0a926a7389697ac1b
 </style>
