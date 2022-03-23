6d2837119ef7ad8b7c4626846c49f1c4230f8d0c
Alexis H <75497497+Hexiss@users.noreply.github.com>
2022-03-23 18:29:59 +0100

    acceuil

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,10 +1,89 @@
 <template>
-    <div id="app">
-      <h1>test</h1>
-        <img src="../assets/moon.png">
-        <img src="../assets/rat.png">
-    </div>
+<body>
+  <div>
+<div class="flex">
+  <div>
+  <img src="@/assets/logos/lune.svg " alt="">
+  </div>
+  <div>
+  <h1 class="title">Podcasts</h1>
+  </div>
+  <div class="w-5/6">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full button">Voir tout</button>
+  </div>
+  </div>
+
+  <div class="image">
+    <ul>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+    </ul>
+  </div>
+
+<div class="flex">
+  <div>
+  <img src="@/assets/logos/lune.svg " alt="">
+  </div>
+  <div>
+  <h1 class="title" style="right:13px">Vidéos</h1>
+  </div>
+  <div class="w-5/6">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full button">Voir tout</button>
+  </div>
+  </div>
+
+  <div class="image">
+    <ul>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+    </ul>
+  </div>
+
+<div class="flex">
+  <div>
+  <img src="@/assets/logos/lune.svg " alt="">
+  </div>
+  <div>
+  <h1 class="title" style="left:3px">Thérapeutes</h1>
+  </div>
+  <div class="w-5/6">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full button">Voir tout</button>
+  </div>
+  </div>
+
+  <div class="image">
+    <ul>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+      <li><img src="@/assets/logos/image.svg " alt=""></li>
+    </ul>
+  </div>
+
+<div>
+  <img src="@/assets/logos/app.svg " alt="">
+</div>
+
+  </div>
+</body>
 </template>
 
-<script>
-</script>
+<style scoped>
+.button {
+  left: 600px;
+  background-color: #1C535D;
+}
+.title {
+  top: 3px;
+  font-size: 25px;
+  width: 110px;
+}
+
+li {
+    display: inline-block;
+}
+</style>
